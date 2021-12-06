50e31d1eafe60fffc95f4ba60ca09aafc35081cc
DenAiz <git@github.com:DenisAiz/ftl-jb-001.git>
2021-12-06 12:18:52 +0300

    L006 - static/instance methods example

--- src/com/company/Animals/Dog.java
+++ src/com/company/Animals/Dog.java
@@ -1,4 +1,35 @@
 package com.company.Animals;
 
 public class Dog extends Animal{
+    public Dog(String name, Integer weight) {
+        super(name, weight);
+    }
+
+    public Dog() {
+    }
+
+    /**
+     * Определяем ранизу между текущей позицией и позицией палки.
+     * @param stickPosition - позиция палки
+     */
+
+    public void goToStick(Integer stickPosition){
+        Integer delta = stickPosition - this.currentPosition;
+
+        this.runForward(delta);
+    }
+
+    public void whereTheDog(){
+        System.out.println(this.currentPosition);
+    }
+
+    public static Dog of(String name, Integer weight){
+        return new Dog(name, weight);
+    }
+
+    public static Dog ofHomeLess(Integer weight){
+        Dog dog = new Dog();
+        dog.weight = weight;
+        return dog;
+    }
 }
