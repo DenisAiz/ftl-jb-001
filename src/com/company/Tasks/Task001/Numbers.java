a56ffd553bd02e66235da74635dcfe5f9bed87ef
DenAiz <git@github.com:DenisAiz/ftl-jb-001.git>
2021-12-10 16:11:58 +0300

    Task001 - Numbers

---
+++ src/com/company/Tasks/Task001/Numbers.java
@@ -0,0 +1,9 @@
+package com.company.Tasks.Task001;
+
+public class Numbers {
+    public static void run(){
+        for (int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+    }
+}
